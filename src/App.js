b48761a64175a214beb4b1404cc00629ec685f27
Ozge Sari <ozgesari@Ozges-MacBook-Pro.local>
2023-12-31 15:27:14 +0000

    data-commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
 
 const App = () => {
     return (
         <div>
-            App
-        </div>
+            <h1 className="underline text-3xl">
+                App
+            </h1>
+        </div >
     )
 }
 
